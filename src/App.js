60a03ce78b428a8b6bb054ab92bbd593e7072aff
Anmol Saluja <asaluja00@gmail.com>
2022-08-25 17:07:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,134 @@
+import React, { useRef, useState } from 'react';
+import './App.css';
+
+//import firebase from 'firebase/app'; //older version
+import firebase from 'firebase/compat/app'; //v9
+
+//to use auth
+//import 'firebase/auth'; //older version
+import 'firebase/compat/auth'; //v9
+
+//to use firestore
+//import 'firebase/firestore'; //Older Version
+import 'firebase/compat/firestore'; //v9
+import 'firebase/analytics';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBSb-B5F3QhdGJXG6aQoLFxRzhXgbc_S24",
+  authDomain: "chatapp-fb40b.firebaseapp.com",
+  databaseURL: "https://chatapp-fb40b-default-rtdb.firebaseio.com",
+  projectId: "chatapp-fb40b",
+  storageBucket: "chatapp-fb40b.appspot.com",
+  messagingSenderId: "823373557502",
+  appId: "1:823373557502:web:8477d8c35bdaf1dccfbfaf",
+  measurementId: "G-8YD88NYCQ5"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+
+
+function App() {
+
+  const [user] = useAuthState(auth);
+
+  return (
+    <div className="App">
+      <header>
+        <h1>⚛️🔥💬</h1>
+        <SignOut />
+      </header>
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
+    </div>
+  );
+}
+
+function SignIn() {
+
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+  return (
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+      <p>Do not violate the community guidelines or you will be banned for life!</p>
+    </>
+  )
+
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+
+function ChatRoom() {
+  const dummy = useRef();
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(25);
+
+  const [messages] = useCollectionData(query, { idField: 'id' });
+
+  const [formValue, setFormValue] = useState('');
+
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    })
+
+    setFormValue('');
+    dummy.current.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  return (<>
+    <main>
+
+      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+
+      <span ref={dummy}></span>
+
+    </main>
+
+    <form onSubmit={sendMessage}>
+
+      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
+
+      <button type="submit" disabled={!formValue}>🕊️</button>
+
+    </form>
+  </>)
+}
+
+
+function ChatMessage(props) {
+  const { text, uid, } = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+  return (<>
+    <div className={`message ${messageClass}`}>
+      <img src={ 'https://iconape.com/wp-content/files/im/10836/png/iconfinder_3_avatar_2754579.png'} />
+      <p>{text}</p>
+    </div>
+  </>)
+}
+
+
+export default App;
